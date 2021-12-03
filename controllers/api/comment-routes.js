0e4881f66cf2521c8a6afcc59772f511308b0af2
sonali_sohoni <ssohoni@camsbycbs.com>
2021-12-03 13:49:42 -0500

    Worked on backend ,models and routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,82 @@
+const router = require("express").Router();
+const { Comment, Post, User } = require("../../models");
+
+router.get("/", (req, res) => {
+	Comment.findAll()
+		.then((dbCommentData) => res.json(dbCommentData))
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+router.post("/", (req, res) => {
+	// expects => {commen	t_text: "This is the comment", user_id: 1, post_id: 2}
+	Comment.create({
+		comment_text: req.body.comment_text,
+		user_id: req.session.user_id,
+		post_id: req.body.post_id,
+	})
+		.then((dbCommentData) => res.json(dbCommentData))
+		.catch((err) => {
+			console.log(err);
+			res.status(400).json(err);
+		});
+});
+
+router.delete("/:id", (req, res) => {
+	Comment.destroy({
+		where: {
+			id: req.params.id,
+		},
+	})
+		.then((dbCommentData) => {
+			if (!dbCommentData) {
+				res.status(404).json({ message: "No comment found with this id!" });
+				return;
+			}
+			res.json(dbCommentData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+router.get("/:id", (req, res) => {
+	Comment.findOne({
+		attributes: { exclude: ["password"] },
+		where: {
+			id: req.params.id,
+		},
+		include: [
+			{
+				model: Post,
+				attributes: ["id", "title", "content", "created_at"],
+			},
+			{
+				model: User,
+				attributes: ["id", "username"],
+			},
+			// {
+			// 	model: Post,
+			// 	attributes: ["title"],
+			// 	through: Vote,
+			// 	as: "voted_posts",
+			// },
+		],
+	})
+		.then((dbUserData) => {
+			if (!dbUserData) {
+				res.status(404).json({ message: "No user found with this id" });
+				return;
+			}
+			res.json(dbUserData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+module.exports = router;
